7399acf0077058a72e11908702e599fb805c6ffc
Takayuki Muranushi <muranushi@gmail.com>
2017-01-06 23:11:59 +0900

    Runner

--- retz/run-python.sh
+++ retz/run-python.sh
@@ -1,1 +1,8 @@
-/opt/retz-client/bin/retz-client  run -A nushio -c 'python 01-get-sun-image.py' --stderr  --mem 16000 --gpu 1 --cpu 8 -E CUDA_PATH=/usr/local/cuda
+# run GPU job
+# retz-client run -A nushio -c 'python 01-get-sun-image.py' --stderr  --mem 16000 --gpu 1 --cpu 8 -E CUDA_PATH=/usr/local/cuda
+
+# run CPU job
+# retz-client run -A nushio -c 'python 01-get-sun-image.py' --stderr  --mem 1000 --cpu 1
+# retz-client schedule -A nushio -c 'python 01-get-sun-image.py'  --mem 1000 --cpu 1
+# retz-client schedule -A nushio -c 'python 04-perfect-memoization.py'  --mem 6000 --cpu 1
+retz-client run -A nushio4 -c 'python 04-perfect-memoization.py' --stderr --mem 6000 --cpu 1
