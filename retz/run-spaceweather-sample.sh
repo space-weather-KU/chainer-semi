3f53cd296af0fab11438109efcff176ee885dad9
Takayuki Muranushi <muranushi@gmail.com>
2017-02-17 20:33:41 +0900

    Waboku

--- retz/run-spaceweather-sample.sh
+++ retz/run-spaceweather-sample.sh
@@ -1,2 +1,2 @@
-retz-client run -A movie-dcgan-2 -c 'wget https://raw.githubusercontent.com/space-weather-KU/chainer-semi/master/learn-sun/nushio3/19-movie-dcgan.py -O main.py && python main.py' --mem 6000 --cpu 1 --stderr
+retz-client run -A movie-dcgan-2 -c 'touch stdout && wget https://raw.githubusercontent.com/space-weather-KU/chainer-semi/master/learn-sun/nushio3/19-movie-dcgan.py -O main.py && python main.py' --mem 6000 --cpu 1 --stderr
 # apt-get update && apt-get -y install zip &&
