19669cb5549fbd09cfa122694c61a23bfab81db0
Takayuki Muranushi <muranushi@gmail.com>
2017-02-17 18:46:43 +0900

    User movie dcgan

--- retz/run-spaceweather-sample.sh
+++ retz/run-spaceweather-sample.sh
@@ -1,1 +1,1 @@
-retz-client run -A movie-predict-nushio -c 'apt-get update && apt-get -y install zip && python 16-movie-dcgan.py' --mem 6000 --cpu 1 --stderr
+retz-client run -A movie-dcgan -c 'apt-get update && apt-get -y install zip && python 16-movie-dcgan.py' --mem 6000 --cpu 1 --stderr
