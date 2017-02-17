f5328117d437d92f76ff33da36d8d86e26b9bcab
Takayuki Muranushi <muranushi@gmail.com>
2017-02-17 18:59:14 +0900

    Change

--- retz/load-app.sh
+++ retz/load-app.sh
@@ -1,4 +1,4 @@
 retz-client load-app -A movie-dcgan-2 \
   --container docker --image nushio3/chainer-semi \
-  -F https://raw.githubusercontent.com/space-weather-KU/chainer-semi/master/learn-sun/nushio3/16-movie-dcgan.py \
+  -F https://raw.githubusercontent.com/space-weather-KU/chainer-semi/master/learn-sun/nushio3/17-movie-dcgan.py \
   --user root
--- retz/run-spaceweather-sample.sh
+++ retz/run-spaceweather-sample.sh
@@ -1,2 +1,2 @@
-retz-client run -A movie-dcgan-2 -c 'python 16-movie-dcgan.py' --mem 6000 --cpu 1 --stderr
+retz-client run -A movie-dcgan-2 -c 'python 17-movie-dcgan.py' --mem 6000 --cpu 1 --stderr
 # apt-get update && apt-get -y install zip &&
